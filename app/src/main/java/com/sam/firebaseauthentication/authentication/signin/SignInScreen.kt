4234bca839f85378939ec7b3d5c863947c917668
mac <@Fleur41>
2025-05-14 19:39:06 +0300

    Updated SignUpScreen

--- app/src/main/java/com/sam/firebaseauthentication/authentication/signin/SignInScreen.kt
+++ app/src/main/java/com/sam/firebaseauthentication/authentication/signin/SignInScreen.kt
@@ -1,15 +1,23 @@
 package com.sam.firebaseauthentication.authentication.signin
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,7 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import com.sam.firebaseauthentication.components.VerticalSpacer
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,21 +61,73 @@ fun SignInScreenContent(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(
+        CustomTextField(
             modifier = Modifier.fillMaxWidth(),
             value = email,
-            onValueChange = {email = it}
+            onValueChange = {email = it},
+            placeholderText = "Enter your Email",
+            onClear = {email = ""}
         )
 
-        TextField(
+        VerticalSpacer(8)
+//        CustomTextField(
+//            modifier = Modifier.fillMaxWidth(),
+//            value = password,
+//            onValueChange = {password = it},
+//            placeholderText = "Enter your Password",
+//            isPasswordField = true,
+//            onClear = {password = ""}
+//        )
+//
+//        VerticalSpacer(8)
+        CustomTextField(
             modifier = Modifier.fillMaxWidth(),
             value = password,
-            onValueChange = {password = it}
+            onValueChange = {password = it},
+            placeholderText = "Enter your Password",
+            isPasswordField = true,
+            onClear = {password = ""}
         )
+
+
+        VerticalSpacer(16)
 
         Button(onClick = {}) {
             Text(text = "Sign In")
         }
     }
+}
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholderText: String,
+    isPasswordField: Boolean = false,
+    onClear: () -> Unit
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text(placeholderText) },
+        shape = RoundedCornerShape(16.dp),
+        visualTransformation = if (isPasswordField) PasswordVisualTransformation() else VisualTransformation.None,
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = value.isNotEmpty(),
+                enter = expandHorizontally(expandFrom = Alignment.Start),
+                exit = shrinkHorizontally(shrinkTowards = Alignment.Start)
+            ) {
+                IconButton(onClick = onClear) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear"
+                    )
+                }
+            }
+        }
+    )
 }
 
